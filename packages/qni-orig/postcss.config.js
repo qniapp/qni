78244f1a396bc75c02cab0340844d3d383fb3171
Yasuhito Takamiya <yasuhito@gmail.com>
2020-04-01 11:19:22 +0900

    purgecss を有効にする (#5)

--- packages/qni-orig/postcss.config.js
+++ packages/qni-orig/postcss.config.js
@@ -1,3 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: [
+    "./app/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || []
+})
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
@@ -10,5 +21,6 @@ module.exports = {
       },
       stage: 3
     }),
+    ...(process.env.RAILS_ENV === 'production' ? [purgecss] : []),
   ]
 }
