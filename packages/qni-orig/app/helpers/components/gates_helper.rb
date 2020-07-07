cb5813cb1ab49f2ce4df0299c28541e701f3c205
Yasuhito Takamiya <yasuhito@gmail.com>
2020-07-07 09:45:49 +0900

    docs: swap ゲートヘルパのドキュメントを追加 (#32)

--- packages/qni-orig/app/helpers/components/gates_helper.rb
+++ packages/qni-orig/app/helpers/components/gates_helper.rb
@@ -73,6 +73,19 @@ module Components::GatesHelper
     render 'components/gates/root_not_gate', *options
   end
 
+  # Returns a partial of the swap gate.
+  #
+  #   # Add a label on top of the gate (default - nil)
+  #   swap_gate(label: 'swap')
+  #
+  #   # Activate the wire (default - true)
+  #   swap_gate(wire_active: false)
+  #
+  #   # Connect with upper gate (default - false)
+  #   swap_gate(top: true)
+  #
+  #   # Connect with lower gate (default - false)
+  #   swap_gate(bottom: true)
   def swap_gate(*options)
     render 'components/gates/swap_gate', *options
   end
