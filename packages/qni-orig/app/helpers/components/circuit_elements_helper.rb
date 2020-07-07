0da90e5ca999c64d85575ef542dfe04ffe7569f8
Yasuhito Takamiya <yasuhito@gmail.com>
2020-07-07 15:49:02 +0900

    docs: top_wire & bottom_wire ヘルパのドキュメントを追加 (#35)

--- packages/qni-orig/app/helpers/components/circuit_elements_helper.rb
+++ packages/qni-orig/app/helpers/components/circuit_elements_helper.rb
@@ -1,4 +1,8 @@
 module Components::CircuitElementsHelper
+  def circuit_element_wire(*options)
+    render 'components/circuit_elements/wire', *options
+  end
+
   def circuit_element_label(*options)
     render 'components/circuit_elements/label', *options
   end
