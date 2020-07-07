24ab0f34296cce66e9a1919c9d83f0a8add5f242
Yasuhito Takamiya <yasuhito@gmail.com>
2020-07-07 11:06:01 +0900

    docs: write と readout ヘルパのドキュメントを追加 (#33)

---
+++ packages/qni-orig/test/helpers/components/rw_helper_test.rb
@@ -0,0 +1,89 @@
+require 'test_helper'
+
+class Components::GatesHelperTest < ActionView::TestCase
+  test 'write 0' do
+    assert_dom_equal beautify(<<~ERB), beautify(write(value: 0))
+      <div class="rw gate write rw--0">
+        <div class="rw__wire rw__wire--left"></div>
+        <div class="rw__wire rw__wire--right"></div>
+        <div class="rw__body">
+          <div>
+            |<span class="rw__value">0</span>&#10217;
+          </div>
+        </div>
+      </div>
+    ERB
+  end
+
+  test 'write 1' do
+    assert_dom_equal beautify(<<~ERB), beautify(write(value: 0))
+      <div class="rw gate write rw--0">
+        <div class="rw__wire rw__wire--left"></div>
+        <div class="rw__wire rw__wire--right"></div>
+        <div class="rw__body">
+          <div>
+            |<span class="rw__value">0</span>&#10217;
+          </div>
+        </div>
+      </div>
+    ERB
+  end
+
+  test 'write (labeled)' do
+    assert_dom_equal beautify(<<~ERB), beautify(write(value: 0, label: 'write 0'))
+      <div class="rw gate write rw--0">
+        <div class="rw__wire rw__wire--left"></div>
+        <div class="rw__wire rw__wire--right"></div>
+        <div class="rw__body">
+          <div>
+            |<span class="rw__value">0</span>&#10217;
+          </div>
+        </div>
+        <div class="gate__label">write 0</div>
+      </div>
+    ERB
+  end
+
+  test 'readout 0' do
+    assert_dom_equal beautify(<<~ERB), beautify(readout(value: 0))
+      <div class="rw gate readout rw--0">
+        <div class="rw__wire rw__wire--left"></div>
+        <div class="rw__wire rw__wire--right"></div>
+        <div class="rw__body">
+          <div>
+            |<span class="rw__value">0</span>&#10217;
+          </div>
+        </div>
+      </div>
+    ERB
+  end
+
+  test 'readout 1' do
+    assert_dom_equal beautify(<<~ERB), beautify(readout(value: 0))
+      <div class="rw gate readout rw--0">
+        <div class="rw__wire rw__wire--left"></div>
+        <div class="rw__wire rw__wire--right"></div>
+        <div class="rw__body">
+          <div>
+            |<span class="rw__value">0</span>&#10217;
+          </div>
+        </div>
+      </div>
+    ERB
+  end
+
+  test 'readout (labeled)' do
+    assert_dom_equal beautify(<<~ERB), beautify(readout(value: 0, label: 'alice_v'))
+      <div class="rw gate readout rw--0">
+        <div class="rw__wire rw__wire--left"></div>
+        <div class="rw__wire rw__wire--right"></div>
+        <div class="rw__body">
+          <div>
+            |<span class="rw__value">0</span>&#10217;
+          </div>
+        </div>
+        <div class="gate__label">alice_v</div>
+      </div>
+    ERB
+  end
+end
