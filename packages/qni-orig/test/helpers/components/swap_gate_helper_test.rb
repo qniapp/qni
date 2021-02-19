06661dd43640c1fe2b81ba169d2a063e852a3c49
Yasuhito Takamiya <yasuhito@gmail.com>
2021-02-19 14:38:00 +0900

    test: swap_gate ヘルパのテストを追加 (#51)

---
+++ packages/qni-orig/test/helpers/components/swap_gate_helper_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class Components::ControlGateHelperTest < ActionView::TestCase
+  include Components::GatesHelper
+  include IconsHelper
+
+  test 'swap_gate' do
+    assert_dom_equal beautify(<<~ERB), beautify(swap_gate(bit: 0, targets: [0, 1]))
+      <div class="instruction gate swap-gate gate--connected-with-upper-bit" data-targets="0,1">
+        <div class="connection connection--top"></div>
+        <div class="connection connection--bottom"></div>
+        <div class="gate__shape gate__shape--swap">
+          #{swap_icon}
+        </div>
+      </div>
+    ERB
+  end
+end
