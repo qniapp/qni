89a7b8dfa1a1cda1250ae89e1559302369a65ac3
Yasuhito Takamiya <yasuhito@gmail.com>
2021-03-06 10:50:28 +0900

    test: controlGate のテストを追加 (#68)

--- packages/qni-orig/test/helpers/components/control_gate_helper_test.rb
+++ packages/qni-orig/test/helpers/components/control_gate_helper_test.rb
@@ -14,4 +14,16 @@ class Components::ControlGateHelperTest < ActionView::TestCase
       </div>
     ERB
   end
+
+  test 'control_gate(targets: [1, 2])' do
+    assert_dom_equal beautify(<<~ERB), beautify(control_gate(targets: [1, 2]))
+      <div class="gate control-gate" data-targets="1,2">
+        <div class="connection connection--top"></div>
+        <div class="connection connection--bottom"></div>
+        <div class="gate__shape gate__shape--square">
+          <div class="control-gate__dot"></div>
+        </div>
+      </div>
+    ERB
+  end
 end
