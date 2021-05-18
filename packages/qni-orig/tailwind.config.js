e67d5498dfdfc7e6798c5a6d11b030fe7de1f35b
Yasuhito Takamiya <yasuhito@gmail.com>
2021-05-18 16:46:30 +0900

    chore: tailwind css 2.1 (#74)

--- packages/qni-orig/tailwind.config.js
+++ packages/qni-orig/tailwind.config.js
@@ -1,31 +1,16 @@
 module.exports = {
-  purge: {
-    content: ["./app/**/*.html.erb", "./app/helpers/**/*.rb"],
-    options: {
-      whitelist: ["qubit-circle-octet", "text-center", "font-bold"],
-      whitelistPatterns: [
-        /-gate$/,
-        /^circle-notation--\d+qubit/,
-        /^circuit/,
-        /^pill/,
-        /^qubits-\d+/,
-        /^readout--/,
-        /^reg-end-\d+/,
-        /^reg-start-\d+/,
-        /^simulator-wrapper--\d+qubit/,
-        /^span-\d+/,
-        /^write--/,
-      ],
-    },
-  },
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       spacing: {
-        "9": "2.25rem",
-        "18": "4.5rem",
+        9: "2.25rem",
+        18: "4.5rem",
       },
     },
   },
-  variants: {},
-  plugins: [require("@tailwindcss/ui")],
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
