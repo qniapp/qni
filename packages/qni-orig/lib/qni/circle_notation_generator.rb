f82af6676e932de6df87c35685f2308351f9712c
Yasuhito Takamiya <yasuhito@gmail.com>
2021-05-28 09:15:54 +0900

    fix: 状態ベクトル表示を修正 (#94)

--- packages/qni-orig/lib/qni/circle_notation_generator.rb
+++ packages/qni-orig/lib/qni/circle_notation_generator.rb
@@ -4,8 +4,6 @@ module Qni
   class CircleNotationGenerator
     include ComponentsHelper
 
-    MAX_NQUBIT = 8
-
     def initialize(circuit, view)
       @circuit = circuit
       @view = view
@@ -20,38 +18,40 @@ module Qni
 
     private
 
+    MAX_NQUBIT = 8
+
     def body
       component 'state_vector', view: @view do
         state_vector_row.html_safe # rubocop:disable Rails/OutputSafety
       end
     end
 
+    # rubocop:disable Metrics/AbcSize
+    # rubocop:disable Metrics/MethodLength
+    # rubocop:disable Rails/OutputSafety
     def state_vector_row
-      if @dsl.nqubit > 3
-        large_state_vector_row
-      else
-        small_state_vector_row
-      end
-    end
-
-    def small_state_vector_row
-      circles.in_groups_of(16, false).map do |slice|
-        component 'state_vector/row', view: @view do
-          slice.join.html_safe # rubocop:disable Rails/OutputSafety
+      circles.in_groups_of(64, false).map do |g64|
+        component('qubit_circle_group', size: 64, view: @view) do
+          g64.in_groups_of(32, false).map do |g32|
+            component('qubit_circle_group', size: 32, view: @view) do
+              g32.in_groups_of(16, false).map do |g16|
+                component('qubit_circle_group', size: 16, view: @view) do
+                  if g16.length < 9
+                    component('qubit_circle_group', size: 8, view: @view) { g16[0..7].join.html_safe }
+                  else
+                    component('qubit_circle_group', size: 8, view: @view) { g16[0..7].join.html_safe } +
+                      component('qubit_circle_group', size: 8, view: @view) { g16[8..15].join.html_safe }
+                  end
+                end
+              end.join.html_safe
+            end
+          end.join.html_safe
         end
       end.join
     end
-
-    def large_state_vector_row
-      circles.in_groups_of(16, false).map do |slice|
-        component 'state_vector/row', view: @view do
-          <<~HTML.html_safe # rubocop:disable Rails/OutputSafety
-            <div class="qubit-circle-octet">#{slice[0..7].join}</div>
-            <div class="qubit-circle-octet">#{slice[8..15].join}</div>
-          HTML
-        end
-      end.join
-    end
+    # rubocop:enable Metrics/AbcSize
+    # rubocop:enable Metrics/MethodLength
+    # rubocop:enable Rails/OutputSafety
 
     def circles
       (0..2**MAX_NQUBIT - 1).map do |ket|
