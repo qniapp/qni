3b4ce1322ca1df1fdb28461f6ca2de57b9d0811b
Yasuhito Takamiya <yasuhito@gmail.com>
2021-06-03 11:42:29 +0900

    fix: up-down を swap に変更 (#111)

--- packages/qni-orig/app/components/gates/swap_gate_component.rb
+++ packages/qni-orig/app/components/gates/swap_gate_component.rb
@@ -1,10 +1,12 @@
 require 'component'
 require 'concerns/connectable'
+require 'concerns/controllable'
 require 'concerns/targetable'
 require 'concerns/wireable'
 
 class Gates::SwapGateComponent < Component
   include Connectable
+  include Controllable
   include Targetable
   include Wireable
 
@@ -16,19 +18,27 @@ class Gates::SwapGateComponent < Component
                  'instruction--wire-inactive' => !wire_active?)
   end
 
+  # rubocop:disable Metrics/AbcSize
   def data
-    { targets: targets.join(',') }
+    if !targets.empty? && !controls.empty?
+      { targets: targets.join(','), controls: controls.join(',') }
+    elsif !targets.empty? && controls.empty?
+      { targets: targets.join(',') }
+    elsif targets.empty? && !controls.empty?
+      { controls: controls.join(',') }
+    end
   end
+  # rubocop:enable Metrics/AbcSize
 
   def connected_with_upper_bit?
     return false unless bit
 
-    targets.any? { |each| each > bit }
+    (targets + controls).any? { |each| each > bit }
   end
 
   def connected_with_lower_bit?
     return false unless bit
 
-    targets.any? { |each| each < bit }
+    (targets + controls).any? { |each| each < bit }
   end
 end
