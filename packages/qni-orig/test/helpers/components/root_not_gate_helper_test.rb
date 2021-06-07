ede61dba7f2eef52fc6f3382e848d1668cfb3604
Yasuhito Takamiya <yasuhito@gmail.com>
2021-06-07 10:49:03 +0900

    refactor: ゲートの上下ワイヤを SVG にする

--- packages/qni-orig/test/helpers/components/root_not_gate_helper_test.rb
+++ packages/qni-orig/test/helpers/components/root_not_gate_helper_test.rb
@@ -7,8 +7,10 @@ class Components::RootNotGateHelperTest < ActionView::TestCase
   test 'root_not_gate' do
     assert_dom_equal beautify(<<~ERB), beautify(root_not_gate)
       <div class="gate gate--ifable root-not-gate" data-controller="gate-popup">
-        <div class="connection connection--top"></div>
-        <div class="connection connection--bottom"></div>
+        <svg class="connection overflow-visible" width="100" height="100" viewBox="0 0 100 100" preserveAspectRatio="none">
+          <line class="connection__top" x1="50" y1="0" x2="50" y2="50" stroke-width="2" stroke="currentColor" vector-effect="non-scaling-stroke"></line>
+          <line class="connection__bottom" x1="50" y1="100" x2="50" y2="50" stroke-width="2" stroke="currentColor" vector-effect="non-scaling-stroke"></line>
+        </svg>
         <div class="gate__shape gate__shape--square gate__shape--fill">
           #{root_not_icon}
         </div>
@@ -28,8 +30,10 @@ class Components::RootNotGateHelperTest < ActionView::TestCase
   test 'root_not_gate if:' do
     assert_dom_equal beautify(<<~ERB), beautify(root_not_gate(if: 'alice_rn'))
       <div class="gate gate--ifable root-not-gate" data-if="alice_rn" data-controller="gate-popup">
-        <div class="connection connection--top"></div>
-        <div class="connection connection--bottom"></div>
+        <svg class="connection overflow-visible" width="100" height="100" viewBox="0 0 100 100" preserveAspectRatio="none">
+          <line class="connection__top" x1="50" y1="0" x2="50" y2="50" stroke-width="2" stroke="currentColor" vector-effect="non-scaling-stroke"></line>
+          <line class="connection__bottom" x1="50" y1="100" x2="50" y2="50" stroke-width="2" stroke="currentColor" vector-effect="non-scaling-stroke"></line>
+        </svg>
         <div class="gate__shape gate__shape--square gate__shape--fill">
           #{root_not_icon}
         </div>
