295d655eb90c81b73e58c794ed00e8bc6cec9f73
Yasuhito Takamiya <yasuhito@gmail.com>
2021-06-18 06:10:58 +0900

    perf: circle notation 描画を軽くする (#144)

--- packages/qni-orig/app/javascript/packs/application.js
+++ packages/qni-orig/app/javascript/packs/application.js
@@ -1,3 +1,5 @@
+/* eslint no-unused-vars: "off" */
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -10,7 +12,7 @@ require("turbolinks").start()
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
+const images = require.context("../images", true)
 // const imagePath = (name) => images(name, true)
 
 import "controllers"
--- packages/qni-orig/app/javascript/service_workers/service-worker.js
+++ packages/qni-orig/app/javascript/service_workers/service-worker.js
@@ -32,3 +32,12 @@ self.addEventListener(
   },
   false,
 )
+
+self.addEventListener("install", function(event) {
+})
+
+self.addEventListener("activate", function(event) {
+})
+
+self.addEventListener("fetch", function(event) {
+})
