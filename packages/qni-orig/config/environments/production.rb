7ea9efae97e1b92605b9aa6483b33d8721b067d2
Yasuhito Takamiya <yasuhito@gmail.com>
2021-06-21 08:24:57 +0900

    chore: cloudfront を有効にする (#148)

--- packages/qni-orig/config/environments/production.rb
+++ packages/qni-orig/config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = 'd3zpidr1zpoa6.cloudfront.net'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
