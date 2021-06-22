4251e95096b8b703e8ffe17ce882416eb079ceeb
Yasuhito Takamiya <yasuhito@gmail.com>
2021-06-22 11:53:09 +0900

    feat: デフォルトでエディタを開く (#155)

--- packages/qni-orig/config/routes.rb
+++ packages/qni-orig/config/routes.rb
@@ -1,6 +1,7 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  root 'circuits#index'
+  root 'circuits#show'
+
   resources :circuits, only: %i[index show]
   resources :circuit_previews, only: :show
 
