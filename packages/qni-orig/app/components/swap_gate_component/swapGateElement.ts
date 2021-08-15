af108539cd8d1953c81b61dad93199f0b1728228
Yasuhito Takamiya <yasuhito@gmail.com>
2021-08-15 12:07:27 +0900

    feat: swap ゲートのサイズ指定等 (#230)

--- packages/qni-orig/app/components/swap_gate_component/swapGateElement.ts
+++ packages/qni-orig/app/components/swap_gate_component/swapGateElement.ts
@@ -1,3 +1,4 @@
+import tippy, { Instance, ReferenceElement, roundArrow } from "tippy.js"
 import { controller, attr, target } from "@github/catalyst"
 import { html, render } from "@github/jtml"
 
@@ -46,9 +47,13 @@ const swapIcon = html`<svg
 export class SwapGateElement extends HTMLElement {
   @target body: HTMLElement
 
-  @attr wireTop = false
-  @attr wireBottom = false
+  @attr size = "base"
   @attr disabled = false
+  @attr wireTop = false
+  @attr wireTopDisabled = false
+  @attr wireBottom = false
+  @attr wireBottomDisabled = false
+  @attr draggable = false
 
   disable(): void {
     this.disabled = true
@@ -56,6 +61,30 @@ export class SwapGateElement extends HTMLElement {
 
   enable(): void {
     this.disabled = false
+  }
+
+  showGateDescription(): void {
+    if ((this as ReferenceElement)._tippy) return
+
+    const content = this.descriptionHeader()
+    if (!content) return
+
+    const popup = tippy(this, {
+      allowHTML: true,
+      animation: false,
+      arrow: roundArrow + roundArrow,
+      delay: 0,
+      placement: "right",
+      theme: "qni",
+      onShow(instance: Instance) {
+        instance.setContent(content)
+      },
+    })
+    popup.show()
+  }
+
+  private descriptionHeader(): HTMLElement | null {
+    return this.querySelector("header")
   }
 
   connectedCallback(): void {
@@ -68,14 +97,18 @@ export class SwapGateElement extends HTMLElement {
     oldValue: string | null,
     newValue: string | null,
   ): void {
-    if (name === "data-wire-top" && oldValue !== newValue && this.body) {
-      this.body.classList.add(this.wireTopClassString)
+    if (!this.body) return
+    if (oldValue === newValue) return
+
+    if (name === "data-disabled") {
+      this.body.classList.toggle("disabled")
     }
-    if (name === "data-wire-bottom" && oldValue !== newValue && this.body) {
-      this.body.classList.add(this.wireBottomClassString)
+
+    if (name === "data-wire-top") {
+      this.body.classList.toggle("wire-top")
     }
-    if (name === "data-disabled" && oldValue !== newValue && this.body) {
-      this.body.classList.add(this.disabledClassString)
+    if (name === "data-wire-bottom") {
+      this.body.classList.toggle("wire-bottom")
     }
   }
 
@@ -91,6 +124,54 @@ export class SwapGateElement extends HTMLElement {
             width: 2rem;
           }
 
+          #body.size-xs {
+            height: 1rem;
+            width: 1rem;
+          }
+
+          #body.size-sm {
+            height: 1.5rem;
+            width: 1.5rem;
+          }
+
+          #body.size-base {
+            height: 2rem;
+            width: 2rem;
+          }
+
+          #body.size-lg {
+            height: 2.5rem;
+            width: 2.5rem;
+          }
+
+          #body.size-xl {
+            height: 3rem;
+            width: 3rem;
+          }
+
+          #body.draggable {
+            cursor: grab;
+          }
+
+          #body.draggable::after {
+            position: absolute;
+            top: 0px;
+            right: 0px;
+            bottom: 0px;
+            left: 0px;
+            border-color: var(--colors-cardinal, #ff4b4b);
+            border-radius: 0.25rem;
+            border-style: solid;
+            border-width: 2px;
+            box-sizing: border-box;
+            opacity: 0;
+            content: "";
+          }
+
+          #body:hover::after {
+            opacity: 100;
+          }
+
           #wires {
             position: absolute;
             bottom: -2px;
@@ -104,19 +185,34 @@ export class SwapGateElement extends HTMLElement {
 
           #wire-top,
           #wire-bottom {
-            color: var(--colors-gate, #43c000);
             stroke-width: 4;
             display: none;
           }
 
           #body.wire-top #wire-top {
             display: block;
+            color: var(--colors-gate, #43c000);
+            transform-origin: top;
+            transform: translateY(-25%) scaleY(1.5);
+          }
+
+          #body.wire-top-disabled #wire-top {
+            display: block;
+            color: var(--colors-eel, #4b4b4b);
             transform-origin: top;
             transform: translateY(-25%) scaleY(1.5);
           }
 
           #body.wire-bottom #wire-bottom {
             display: block;
+            color: var(--colors-gate, #43c000);
+            transform-origin: bottom;
+            transform: translateY(25%) scaleY(1.5);
+          }
+
+          #body.wire-bottom-disabled #wire-bottom {
+            display: block;
+            color: var(--colors-eel, #4b4b4b);
             transform-origin: bottom;
             transform: translateY(25%) scaleY(1.5);
           }
@@ -140,9 +236,9 @@ export class SwapGateElement extends HTMLElement {
 
         <div
           id="body"
-          class="${this.wireTopClassString} ${this.wireBottomClassString} ${this
-            .disabledClassString}"
+          class="${this.classString}"
           data-target="swap-gate.body"
+          data-action="mouseenter:swap-gate#showGateDescription"
         >
           ${verticalWires} ${swapIcon}
         </div>`,
@@ -150,15 +246,24 @@ export class SwapGateElement extends HTMLElement {
     )
   }
 
-  private get wireTopClassString(): string {
-    return this.wireTop ? "wire-top" : ""
-  }
+  private get classString(): string {
+    const klass = []
 
-  private get wireBottomClassString(): string {
-    return this.wireBottom ? "wire-bottom" : ""
-  }
+    if (this.size === "xs") klass.push("size-xs")
+    if (this.size === "sm") klass.push("size-sm")
+    if (this.size === "base") klass.push("size-base")
+    if (this.size === "lg") klass.push("size-lg")
+    if (this.size === "xl") klass.push("size-xl")
 
-  private get disabledClassString(): string {
-    return this.disabled ? "disabled" : ""
+    if (this.disabled) klass.push("disabled")
+
+    if (this.wireTop) klass.push("wire-top")
+    if (this.wireTopDisabled) klass.push("wire-top-disabled")
+    if (this.wireBottom) klass.push("wire-bottom")
+    if (this.wireBottomDisabled) klass.push("wire-bottom-disabled")
+
+    if (this.draggable) klass.push("draggable")
+
+    return klass.join(" ")
   }
 }
