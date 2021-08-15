2720c1c0f69f514e9232abe605cf9f9ae2896fa6
Yasuhito Takamiya <yasuhito@gmail.com>
2021-08-15 16:42:18 +0900

    docs: ドキュメントを追加

--- packages/qni-orig/app/components/control_gate_component/controlGateElement.ts
+++ packages/qni-orig/app/components/control_gate_component/controlGateElement.ts
@@ -1,3 +1,4 @@
+import tippy, { Instance, ReferenceElement, roundArrow } from "tippy.js"
 import { controller, attr, target } from "@github/catalyst"
 import { html, render } from "@github/jtml"
 
@@ -48,6 +49,7 @@ const controlDotIcon = html`
 export class ControlGateElement extends HTMLElement {
   @target body: HTMLElement
 
+  @attr size = "base"
   @attr draggable = false
   @attr draggableSource = false
   @attr draggableShadow = false
@@ -64,6 +66,30 @@ export class ControlGateElement extends HTMLElement {
 
   enable(): void {
     this.disabled = false
+  }
+
+  showGateDescription(): void {
+    if ((this as ReferenceElement)._tippy) return
+
+    const content = this.descriptionHeader()
+    if (!content) return
+
+    const popup = tippy(this, {
+      allowHTML: true,
+      animation: false,
+      arrow: roundArrow + roundArrow,
+      delay: 0,
+      placement: "right",
+      theme: "qni",
+      onShow(instance: Instance) {
+        instance.setContent(content)
+      },
+    })
+    popup.show()
+  }
+
+  private descriptionHeader(): HTMLElement | null {
+    return this.querySelector("header")
   }
 
   connectedCallback(): void {
@@ -122,6 +148,31 @@ export class ControlGateElement extends HTMLElement {
             position: relative;
             height: 2rem;
             width: 2rem;
+          }
+
+          #body.size-xs {
+            height: 1rem;
+            width: 1rem;
+          }
+
+          #body.size-sm {
+            height: 1.5rem;
+            width: 1.5rem;
+          }
+
+          #body.size-base {
+            height: 2rem;
+            width: 2rem;
+          }
+
+          #body.size-lg {
+            height: 2.5rem;
+            width: 2.5rem;
+          }
+
+          #body.size-xl {
+            height: 3rem;
+            width: 3rem;
           }
 
           #body.grabbed {
@@ -226,9 +277,9 @@ export class ControlGateElement extends HTMLElement {
 
         <div
           id="body"
-          class="${this.draggableClassString} ${this.disabledClassString} ${this
-            .wireTopClassString} ${this.wireBottomClassString}"
+          class="${this.classString}"
           data-target="control-gate.body"
+          data-action="mouseenter:control-gate#showGateDescription"
         >
           ${verticalWires} ${controlDotIcon}
         </div>`,
@@ -236,23 +287,24 @@ export class ControlGateElement extends HTMLElement {
     )
   }
 
-  private get draggableClassString(): string {
-    return this.draggable ? "draggable" : ""
-  }
+  private get classString(): string {
+    const klass = []
 
-  private get disabledClassString(): string {
-    return this.disabled ? "disabled" : ""
-  }
+    if (this.size === "xs") klass.push("size-xs")
+    if (this.size === "sm") klass.push("size-sm")
+    if (this.size === "base") klass.push("size-base")
+    if (this.size === "lg") klass.push("size-lg")
+    if (this.size === "xl") klass.push("size-xl")
 
-  private get wireTopClassString(): string {
-    if (this.wireTop) return "wire-top"
-    if (this.wireTopDisabled) return "wire-top-disabled"
-    return ""
-  }
+    if (this.disabled) klass.push("disabled")
 
-  private get wireBottomClassString(): string {
-    if (this.wireBottom) return "wire-bottom"
-    if (this.wireBottomDisabled) return "wire-bottom-disabled"
-    return ""
+    if (this.wireTop) klass.push("wire-top")
+    if (this.wireTopDisabled) klass.push("wire-top-disabled")
+    if (this.wireBottom) klass.push("wire-bottom")
+    if (this.wireBottomDisabled) klass.push("wire-bottom-disabled")
+
+    if (this.draggable) klass.push("draggable")
+
+    return klass.join(" ")
   }
 }
