0ca6598db0b7ba9f471b95501c650acc5ca9277f
Yasuhito Takamiya <yasuhito@gmail.com>
2021-08-18 14:18:19 +0900

    chore: js を小さくする (#238)

--- packages/qni-orig/config/webpack/production.js
+++ packages/qni-orig/config/webpack/production.js
@@ -2,7 +2,31 @@ process.env.NODE_ENV = process.env.NODE_ENV || "production"
 
 const environment = require("./environment")
 
-// FIXME
-environment.config.set("optimization.minimize", false)
+const TerserPlugin = require("terser-webpack-plugin")
+environment.config.set("optimization.minimize", true)
+
+environment.config.delete("optimization.minimizer")
+environment.config.set("optimization.minimizer", [
+  new TerserPlugin({
+    terserOptions: {
+      mangle: {
+        keep_classnames: true,
+        reserved: ["QuantumCircuitElement", "CircuitStepElement", "CircuitDropzoneElement",
+                   "HGateElement",
+                   "XGateElement", "YGateElement", "ZGateElement",
+                   "PhaseGateElement",
+                   "RootNotGateElement",
+                   "RxGateElement", "RyGateElement", "RzGateElement",
+                   "ControlGateElement",
+                   "SwapGateElement",
+                   "BlochDisplayElement",
+                   "WriteGateElement",
+                   "MeasurementGateElement",
+                   "CircleNotationElement",
+                   "RunCircuitButtonElement"]
+      },
+    },
+  }),
+])
 
 module.exports = environment.toWebpackConfig()
