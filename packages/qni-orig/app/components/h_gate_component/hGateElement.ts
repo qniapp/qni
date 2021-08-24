e700fe4aab85048c93db794d06e3b5e99eab28f9
Yasuhito Takamiya <yasuhito@gmail.com>
2021-08-24 10:09:47 +0900

    feat: h ゲートの data-if を追加 (#243)

--- packages/qni-orig/app/components/h_gate_component/hGateElement.ts
+++ packages/qni-orig/app/components/h_gate_component/hGateElement.ts
@@ -76,6 +76,131 @@ const css = html`<style>
     width: 3rem;
   }
 
+  /* IF label */
+  :host::before {
+    position: absolute;
+    color: var(--colors-wolf, #777777);
+    background-color: transparent;
+    font-size: 0.75rem;
+    line-height: 0.75rem;
+    letter-spacing: -0.05em;
+    font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas,
+      "Liberation Mono", "Courier New", monospace;
+    white-space: nowrap;
+    z-index: 10;
+    content: attr(data-if) "";
+  }
+
+  :host(:not([data-wire-top]):not([data-wire-bottom]))::before,
+  :host(:not([data-wire-top]))::before,
+  :host(:not([data-wire-top-disabled]):not([data-wire-bottom-disabled]))::before,
+  :host(:not([data-wire-top-disabled]))::before {
+    bottom: 0px;
+  }
+
+  :host([data-size="xs"]:not([data-wire-top]):not([data-wire-bottom]))::before,
+  :host([data-size="xs"]:not([data-wire-top]))::before,
+  :host([data-size="xs"]:not([data-wire-top-disabled]):not([data-wire-bottom-disabled]))::before,
+  :host([data-size="xs"]:not([data-wire-top-disabled]))::before {
+    margin-bottom: 1.125rem;
+  }
+
+  :host([data-size="sm"]:not([data-wire-top]):not([data-wire-bottom]))::before,
+  :host([data-size="sm"]:not([data-wire-top]))::before,
+  :host([data-size="sm"]:not([data-wire-top-disabled]):not([data-wire-bottom-disabled]))::before,
+  :host([data-size="sm"]:not([data-wire-top-disabled]))::before {
+    margin-bottom: 1.625rem;
+  }
+
+  :host(:not([data-wire-top]))::before,
+  :host([data-size="base"]:not([data-wire-top]):not([data-wire-bottom]))::before,
+  :host([data-size="base"]:not([data-wire-top]))::before,
+  :host(:not([data-wire-top-disabled]))::before,
+  :host([data-size="base"]:not([data-wire-top-disabled]):not([data-wire-bottom-disabled]))::before,
+  :host([data-size="base"]:not([data-wire-top-disabled]))::before {
+    margin-bottom: 2.125rem;
+  }
+
+  :host([data-size="lg"]:not([data-wire-top]):not([data-wire-bottom]))::before,
+  :host([data-size="lg"]:not([data-wire-top]))::before,
+  :host([data-size="lg"]:not([data-wire-top-disabled]):not([data-wire-bottom-disabled]))::before,
+  :host([data-size="lg"]:not([data-wire-top-disabled]))::before {
+    margin-bottom: 2.625rem;
+  }
+
+  :host([data-size="xl"]:not([data-wire-top]):not([data-wire-bottom]))::before,
+  :host([data-size="xl"]:not([data-wire-top]))::before,
+  :host([data-size="xl"]:not([data-wire-top-disabled]):not([data-wire-bottom-disabled]))::before,
+  :host([data-size="xl"]:not([data-wire-top-disabled]))::before {
+    margin-bottom: 3.125rem;
+  }
+
+  :host([data-wire-top]:not([data-wire-bottom]))::before,
+  :host([data-wire-top-disabled]:not([data-wire-bottom-disabled]))::before {
+    top: 0px;
+  }
+
+  :host([data-size="xs"][data-wire-top]:not([data-wire-bottom]))::before,
+  :host([data-size="xs"][data-wire-top-disabled]:not([data-wire-bottom-disabled]))::before {
+    margin-top: 1.125rem;
+  }
+
+  :host([data-size="sm"][data-wire-top]:not([data-wire-bottom]))::before,
+  :host([data-size="sm"][data-wire-top-disabled]:not([data-wire-bottom-disabled]))::before {
+    margin-top: 1.625rem;
+  }
+
+  :host([data-wire-top]:not([data-wire-bottom]))::before,
+  :host([data-size="base"][data-wire-top]:not([data-wire-bottom]))::before,
+  :host([data-wire-top-disabled]:not([data-wire-bottom-disabled]))::before,
+  :host([data-size="base"][data-wire-top-disabled]:not([data-wire-bottom-disabled]))::before {
+    margin-top: 2.125rem;
+  }
+
+  :host([data-size="lg"][data-wire-top]:not([data-wire-bottom]))::before,
+  :host([data-size="lg"][data-wire-top-disabled]:not([data-wire-bottom-disabled]))::before {
+    margin-top: 2.625rem;
+  }
+
+  :host([data-size="xl"][data-wire-top]:not([data-wire-bottom]))::before,
+  :host([data-size="xl"][data-wire-top-disabled]:not([data-wire-bottom-disabled]))::before {
+    margin-top: 3.125rem;
+  }
+
+  :host([data-wire-top][data-wire-bottom])::before,
+  :host([data-wire-top-disabled][data-wire-bottom-disabled])::before {
+    text-shadow: 1px 1px 0 #fff, -1px -1px 0 #fff, -1px 1px 0 #fff,
+      1px -1px 0 #fff, 0px 1px 0 #fff, 0-1px 0 #fff, -1px 0 0 #fff, 1px 0 0 #fff;
+    bottom: 0px;
+  }
+
+  :host([data-size="xs"][data-wire-top][data-wire-bottom])::before,
+  :host([data-size="xs"][data-wire-top-disabled][data-wire-bottom-disabled])::before {
+    margin-bottom: 1.125rem;
+  }
+
+  :host([data-size="sm"][data-wire-top][data-wire-bottom])::before,
+  :host([data-size="sm"][data-wire-top-disabled][data-wire-bottom-disabled])::before {
+    margin-bottom: 1.625rem;
+  }
+
+  :host([data-wire-top][data-wire-bottom])::before,
+  :host([data-size="base"][data-wire-top][data-wire-bottom])::before,
+  :host([data-wire-top-disabled][data-wire-bottom-disabled])::before,
+  :host([data-size="base"][data-wire-top-disabled][data-wire-bottom-disabled])::before {
+    margin-bottom: 2.125rem;
+  }
+
+  :host([data-size="lg"][data-wire-top][data-wire-bottom])::before,
+  :host([data-size="lg"][data-wire-top-disabled][data-wire-bottom-disabled])::before {
+    margin-bottom: 2.625rem;
+  }
+
+  :host([data-size="xl"][data-wire-top][data-wire-bottom])::before,
+  :host([data-size="xl"][data-wire-top-disabled][data-wire-bottom-disabled])::before {
+    margin-bottom: 3.125rem;
+  }
+
   :host([data-draggable]) {
     cursor: grab;
   }
@@ -169,6 +294,7 @@ const css = html`<style>
 export class HGateElement extends HTMLElement {
   @attr size = ""
   @attr disabled = false
+  @attr if = ""
   @attr wireTop = false
   @attr wireTopDisabled = false
   @attr wireBottom = false
@@ -186,8 +312,8 @@ export class HGateElement extends HTMLElement {
   showHelp(): void {
     if ((this as ReferenceElement)._tippy) return
 
-    const content = this.innerHTML
-    if (!content) return
+    const content = this.innerHTML.trim()
+    if (content === "") return
 
     const popup = tippy(this, {
       allowHTML: true,
--- packages/qni-orig/config/webpack/environment.js
+++ packages/qni-orig/config/webpack/environment.js
@@ -3,12 +3,4 @@ const { config, environment } = require("@rails/webpacker")
 const WebpackerPwa = require("webpacker-pwa")
 new WebpackerPwa(config, environment)
 
-// environment.config.output.filename = function (entry) {
-//   if (entry.chunk.name === 'service-worker' || entry.chunk.name === 'npm') {
-//     return '[name].js'
-//   } else {
-//     return '[name]-[contenthash].js'
-//   }
-// }
-
 module.exports = environment
