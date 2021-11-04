e7a2dc7258d0f237f61e6b239e39819bffbe38ed
Yasuhito Takamiya <yasuhito@gmail.com>
2021-11-04 14:20:09 +0900

    refactor: アイコン関係を整理

--- packages/qni-orig/app/helpers/components_helper.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module ComponentsHelper
-  def component(name, attrs = nil, &block)
-    view = attrs ? attrs.fetch(:view, self) : self
-    "#{name}_component".classify.constantize.new(view, attrs, &block).render
-  end
-end
--- packages/qni-orig/app/helpers/icon_helper.rb
+++ packages/qni-orig/app/helpers/icon_helper.rb
@@ -1,27 +1,7 @@
 # frozen_string_literal: true
 
 module IconHelper
-  def minus_icon
-    render 'icons/minus'
-  end
-
-  def arrow_right_icon
-    render 'icons/arrow_right'
-  end
-
-  def exclamation_circle_icon
-    render 'icons/exclamation_circle_icon'
-  end
-
-  def share_icon(options = { class: 'h-5 w-5' })
-    render 'icons/share', options: options
-  end
-
-  def outline_x_icon
-    render 'icons/outline_x'
-  end
-
-  def twitter_icon
-    render 'icons/twitter'
+  def icon(name, options = { class: 'h-5 w-5' })
+    render "icon/#{name}", options: options
   end
 end
