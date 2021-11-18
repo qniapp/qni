aaffebb0343c5ecb81bd37d08027adddac222927
Yasuhito Takamiya <yasuhito@hey.com>
2021-11-18 12:53:25 +0900

    <rx-gate> を追加 (#34)

--- packages/qni-elements/src/operation.ts
+++ packages/qni-elements/src/operation.ts
@@ -3,6 +3,7 @@ import {HGateElement} from './h-gate-element'
 import {MeasurementGateElement} from './measurement-gate-element'
 import {PhaseGateElement} from './phase-gate-element'
 import {RnotGateElement} from './rnot-gate-element'
+import {RxGateElement} from './rx-gate-element'
 import {WriteGateElement} from './write-gate-element'
 import {XGateElement} from './x-gate-element'
 import {YGateElement} from './y-gate-element'
@@ -15,6 +16,7 @@ export type Operation =
   | ZGateElement
   | PhaseGateElement
   | RnotGateElement
+  | RxGateElement
   | ControlGateElement
   | WriteGateElement
   | MeasurementGateElement
@@ -25,6 +27,7 @@ export type ControllableOperation =
   | ZGateElement
   | PhaseGateElement
   | RnotGateElement
+  | RxGateElement
 
 export const isControllableOperation = (arg: unknown): arg is ControllableOperation =>
   arg instanceof HGateElement ||
@@ -32,7 +35,8 @@ export const isControllableOperation = (arg: unknown): arg is ControllableOperat
   arg instanceof YGateElement ||
   arg instanceof ZGateElement ||
   arg instanceof PhaseGateElement ||
-  arg instanceof RnotGateElement
+  arg instanceof RnotGateElement ||
+  arg instanceof RxGateElement
 
 export const isWriteGateElement = (arg: unknown): arg is WriteGateElement =>
   arg !== undefined && arg !== null && arg instanceof WriteGateElement
