44dfa31e5681ee515b8467a2e4bf8d97087104e6
Yasuhito Takamiya <yasuhito@hey.com>
2021-11-25 14:18:18 +0900

    Y ゲートにインスペクタを追加 (#46)

--- packages/qni-elements/src/y-gate-element.ts
+++ packages/qni-elements/src/y-gate-element.ts
@@ -1,10 +1,12 @@
-import {ActivateableMixin, DisableableMixin, DraggableMixin, IconableMixin} from './mixin/'
+import {ActivateableMixin, DisableableMixin, DraggableMixin, IconableMixin, IfableMixin, MenuableMixin} from './mixin/'
 import {html, render} from '@github/jtml'
 import {controller} from '@github/catalyst'
 import {iconYGate} from './icon'
 
 @controller
-export class YGateElement extends DraggableMixin(DisableableMixin(IconableMixin(ActivateableMixin(HTMLElement)))) {
+export class YGateElement extends MenuableMixin(
+  IfableMixin(DraggableMixin(DisableableMixin(IconableMixin(ActivateableMixin(HTMLElement)))))
+) {
   connectedCallback(): void {
     if (this.shadowRoot !== null) return
     this.attachShadow({mode: 'open'})
