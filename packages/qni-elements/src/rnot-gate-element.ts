d10b4d7e2a6bc8f4a5c2baf421f4fe83e80a1bf4
Yasuhito Takamiya <yasuhito@hey.com>
2021-11-25 14:36:16 +0900

    rnot ゲートにインスペクタを追加 (#48)

--- packages/qni-elements/src/rnot-gate-element.ts
+++ packages/qni-elements/src/rnot-gate-element.ts
@@ -1,10 +1,12 @@
-import {ActivateableMixin, DisableableMixin, DraggableMixin, IconableMixin} from './mixin/'
+import {ActivateableMixin, DisableableMixin, DraggableMixin, IconableMixin, IfableMixin, MenuableMixin} from './mixin/'
 import {html, render} from '@github/jtml'
 import {controller} from '@github/catalyst'
 import {iconRnotGate} from './icon'
 
 @controller
-export class RnotGateElement extends DraggableMixin(DisableableMixin(IconableMixin(ActivateableMixin(HTMLElement)))) {
+export class RnotGateElement extends MenuableMixin(
+  IfableMixin(DraggableMixin(DisableableMixin(IconableMixin(ActivateableMixin(HTMLElement)))))
+) {
   connectedCallback(): void {
     if (this.shadowRoot !== null) return
     this.attachShadow({mode: 'open'})
