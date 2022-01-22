6df94cac8c405f09a6e3a74dfeb3a4d97b80e82e
Yasuhito Takamiya <yasuhito@gmail.com>
2022-01-23 08:48:09 +0900

    run-circuit-button に ripple エフェクトを追加

--- packages/elements/src/run-circuit-button-element.ts
+++ packages/elements/src/run-circuit-button-element.ts
@@ -1,78 +1,155 @@
+import {Interpreter, createMachine, interpret} from 'xstate'
 import {TemplateResult, html, render} from '@github/jtml'
-import {controller, target} from '@github/catalyst'
-import {iconReload} from './icon'
+import {attr, controller, target} from '@github/catalyst'
+import {iconReload, iconTailSpin} from './icon'
+import {describe} from '@qni/common'
 
-const ovalLoaderIcon = html`<style>
-    .oval-loader-icon {
-      display: none;
-      height: 60%;
-      width: 60%;
-    }
-
-    #button:disabled > .oval-loader-icon {
-      stroke: currentColor;
-      color: rgb(255, 255, 255);
-      display: block;
-    }
-  </style>
-
-  <svg viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" class="oval-loader-icon">
-    <g fill="none" fill-rule="evenodd">
-      <g transform="translate(1 1)" stroke-width="2">
-        <circle stroke-opacity=".5" cx="18" cy="18" r="18" />
-        <path d="M36 18c0-9.94-8.06-18-18-18">
-          <animateTransform
-            attributeName="transform"
-            type="rotate"
-            from="0 18 18"
-            to="360 18 18"
-            dur="1s"
-            repeatCount="indefinite"
-          />
-        </path>
-      </g>
-    </g>
-  </svg>`
+type RunCircuitButtonContext = Record<string, never>
+type RunCircuitButtonEvent = {type: 'FINISH'} | {type: 'CLICK'; clientX: number; clientY: number}
 
 export class RunCircuitButtonElement extends HTMLElement {
-  @target button!: HTMLInputElement
+  @attr running = false
+  @attr debug = false
 
-  runSimulator(): void {
-    this.disable()
-    this.dispatchEvent(new Event('run-circuit-button-click', {bubbles: true}))
-  }
+  @target body!: HTMLElement
+  @target ripple: HTMLElement | undefined
 
-  disable(): void {
-    this.button.disabled = true
-  }
+  private runCircuitButtonMachine = createMachine<RunCircuitButtonContext, RunCircuitButtonEvent>(
+    {
+      id: 'run-circuit-button',
+      initial: 'unknown',
+      states: {
+        unknown: {
+          always: [
+            {target: 'idle', cond: 'isIdle'},
+            {target: 'running', cond: 'isRunning'}
+          ]
+        },
+        idle: {
+          on: {
+            CLICK: {
+              target: 'running',
+              actions: ['ripple', 'setRunningTrue', 'dispatchClickEvent']
+            }
+          }
+        },
+        running: {
+          on: {
+            FINISH: {
+              target: 'idle',
+              actions: ['setRunningFalse']
+            }
+          }
+        }
+      }
+    },
+    {
+      actions: {
+        setRunningFalse: () => {
+          this.running = false
+        },
+        setRunningTrue: () => {
+          this.running = true
+        },
+        ripple: (_context, event) => {
+          if (event.type !== 'CLICK') return
+          if (this.ripple) this.ripple.remove()
 
-  enable(): void {
-    this.button.disabled = false
-  }
+          const newRipple = document.createElement('span')
+          newRipple.id = 'ripple'
+
+          const diameter = Math.max(this.clientWidth, this.clientHeight)
+          const radius = diameter / 2
+          const rect = this.getBoundingClientRect()
+          const left = rect.left + window.pageXOffset
+          const top = rect.top + window.pageYOffset
+
+          newRipple.setAttribute('part', 'ripple')
+          newRipple.setAttribute('data-target', 'run-circuit-button.ripple')
+          newRipple.style.width = newRipple.style.height = `${diameter}px`
+          newRipple.style.left = `${event.clientX - (left + radius)}px`
+          newRipple.style.top = `${event.clientY - (top + radius)}px`
+
+          this.body.appendChild(newRipple)
+        },
+        dispatchClickEvent: () => {
+          this.dispatchEvent(new Event('run-circuit-button-click', {bubbles: true}))
+        }
+      },
+      guards: {
+        isIdle: () => !this.running,
+        isRunning: () => this.running
+      }
+    }
+  )
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private runCircuitButtonService!: Interpreter<RunCircuitButtonContext, any, RunCircuitButtonEvent>
 
   connectedCallback(): void {
     this.attachShadow({mode: 'open'})
     this.update()
+
+    this.runCircuitButtonService = interpret(this.runCircuitButtonMachine)
+      .onTransition(state => {
+        if (this.debug) {
+          // eslint-disable-next-line no-console
+          console.log(`run-circuit-button: ${describe(state.value)}`)
+        }
+      })
+      .start()
+
+    this.addEventListener('click', this.handleClickEvent)
+  }
+
+  attributeChangedCallback(name: string, oldValue: string | null, newValue: string | null): void {
+    if (oldValue === newValue) return
+
+    if (name === 'data-running' && newValue !== null) {
+      this.runCircuitButtonService.send({type: 'FINISH'})
+    }
   }
 
   update(): void {
     render(
-      html`<button
-        id="button"
-        part="button"
-        type="button"
-        data-action="click:run-circuit-button#runSimulator"
-        data-target="run-circuit-button.button"
-        aria-label="Run circuit"
-      >
-        ${this.iconReloadHtml} ${ovalLoaderIcon}
-      </button>`,
+      html`<style>
+          #body {
+            position: relative;
+            overflow: hidden;
+            border-radius: 9999px;
+            height: 100%;
+            width: 100%;
+          }
+
+          #ripple {
+            position: absolute;
+            border-radius: 9999px;
+            transform: scale(0);
+            animation: ripple 400ms linear;
+          }
+
+          @keyframes ripple {
+            to {
+              transform: scale(4);
+              opacity: 0;
+            }
+          }
+        </style>
+
+        <div id="body" part="body" data-target="run-circuit-button.body">${this.reloadIcon} ${this.tailSpinIcon}</div>`,
       this.shadowRoot!
     )
   }
 
-  private get iconReloadHtml(): TemplateResult {
+  private handleClickEvent(event: MouseEvent): void {
+    this.runCircuitButtonService.send({type: 'CLICK', clientX: event.clientX, clientY: event.clientY})
+  }
+
+  private get reloadIcon(): TemplateResult {
     return html([iconReload.data] as unknown as TemplateStringsArray)
+  }
+
+  private get tailSpinIcon(): TemplateResult {
+    return html([iconTailSpin.data] as unknown as TemplateStringsArray)
   }
 }
 
