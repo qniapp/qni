1fdf83198441cf7251fd1905e5cd9994acc420ce
Yasuhito Takamiya <yasuhito@gmail.com>
2022-01-24 11:07:13 +0900

    パーシャルを整理

--- apps/www/app/helpers/application_helper.rb
+++ apps/www/app/helpers/application_helper.rb
@@ -15,4 +15,24 @@ module ApplicationHelper
   def menu
     render 'menu'
   end
+
+  def palette
+    render 'palette'
+  end
+
+  def palette_md
+    render 'palette_md'
+  end
+
+  def palette_help_templates
+    render 'palette_help_templates'
+  end
+
+  def circle_notation
+    render 'circle_notation'
+  end
+
+  def run_circuit_button(options)
+    render 'run_circuit_button', options: options
+  end
 end
