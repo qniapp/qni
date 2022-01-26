521765909b4c12b98607358003d70fe4e018544c
Yasuhito Takamiya <yasuhito@gmail.com>
2022-01-26 09:53:05 +0900

    fix

--- apps/www/config/environments/production.rb
+++ apps/www/config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'd1e9oclfpli04b.cloudfront.net'
+  config.action_controller.asset_host = 'd3s7gfd15xwlq4.cloudfront.net'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
