7c5350d9fe216a1b22459c4e0546137edea1043c
NAKATA Maho <maho.nakata@gmail.com>
2022-02-14 12:47:09 +0900

    Cuquantum backend (#84)
    * Add Docker of cuStatevector version (WIP)
    
    * Small fixes
    
    * update Dockerfile
    
    * Now use qsim as the backend. Looks like cuquantum is too primitive at the moment
    and cannot install outside of conda.
    
    * WIP: cannot install cuQuantum at the moment
    
    * .
    
    * a small fix and now cuquantum builds.
    
    * Cleanups.
    
    * qsim_nvidia -> cirq
    
    * fix cuQuantum download url
    
    * add pycall for gem
    
    * move
    
    * .
    
    * avoid the following error:
    UnicodeEncodeError: 'ascii' codec can't encode characters in position 0-2: ordinal not in range(128)
    
    * test circuit
    
    * .
    
    * Update qni-cirqbridge.
    
    * qsim GPU support requires to build from the source
    
    * Updated cirqbridge. qsimcirq now runs on the GPU.
    
    * Remove empty circuits.
    
    * .
    
    * .
    
    * rename to cirq to cirq_backend
    
    * Solve conflicts when merging from upstream
    
    * Solve conflicts when merging from upstream
    
    * back to my backend.
    
    * .
    
    * backout to my cirqbridge.
    
    * .
    
    * Impliment |0>, |1> initializations and H gate.
    
    Co-authored-by: Yasuhito Takamiya <yasuhito@hey.com>

--- apps/www/lib/cirq.rb
+++ apps/www/lib/cirq.rb
@@ -1,3 +1,92 @@
+require 'pycall/import'
+include PyCall::Import
+
+PyCall.exec(<<~PYTHON)
+import cirq
+import io,sys
+import numpy as np
+import qsimcirq
+from sympy import *
+from sympy.parsing.sympy_parser import parse_expr, standard_transformations, implicit_multiplication_application, convert_xor
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+class cirqbridge:
+    def __init__(self):
+        return
+
+    def run_simulation(self,numofqubits,_circuit_from_qni):
+        print("python cirqbridge start")
+        sys.stdout.flush()
+        transformations = (standard_transformations + (implicit_multiplication_application,) + (convert_xor,))
+        circuit_from_qni = []
+        for a in _circuit_from_qni:
+            if len(a) != 0:
+                circuit_from_qni.append(a)
+        print(circuit_from_qni)
+        sys.stdout.flush()
+        qubits = cirq.LineQubit.range(numofqubits)
+        c = cirq.Circuit()
+        i = 0
+        m = 0
+        measurement = []
+        for column_qni in circuit_from_qni:
+            print("circuit column", i, column_qni)
+            i = i + 1
+            j = 0
+            for circuit_qni in column_qni:
+                j = j + 1
+                print("procssing circit ...")
+                print(column_qni)
+                sys.stdout.flush()
+                if circuit_qni['type'] == u'H':
+                    targetqubits=[ qubits[index] for index in circuit_qni['targets'] ]
+                    if not "controls" in circuit_qni:
+                        c.append([ cirq.H(index) for index in targetqubits] )
+                    else:
+                        controledqubits=[ qubits[index] for index in circuit_qni['controls'] ]
+                        c.append([ cirq.ControlledOperation(controledqubits, cirq.H(index)) for index in targetqubits ])
+                elif circuit_qni['type'] == u'|0>':
+                    targetqubits=[ qubits[index] for index in circuit_qni['targets'] ]
+                    c.append([ cirq.ops.reset(index) for index in targetqubits] )
+                elif circuit_qni['type'] == u'|1>':
+                    targetqubits=[ qubits[index] for index in circuit_qni['targets'] ]
+                    c.append([ cirq.ops.reset(index) for index in targetqubits] )
+                    c.append([ cirq.X(index) for index in targetqubits] )
+                elif circuit_qni['type'] == u'':
+                    pass #nop
+                else:
+                    print("unsupported gate", circuit_qni['type'])
+            print("circuit column", column_qni)
+
+        print("")
+        print('Cirq circiut')
+        print(c)
+        cirq_simulator = cirq.Simulator()
+        cirq_result = cirq_simulator.simulate(c)
+        print('Cirq result:')
+        print(cirq_result)
+        sys.stdout.flush()
+
+        qsim_simulator = qsimcirq.QSimSimulator()
+        qsim_result = qsim_simulator.simulate(c)
+        print('qsim result:')
+        print(qsim_result)
+        sys.stdout.flush()
+
+        gpu_options = qsimcirq.QSimOptions(use_gpu=True)
+        qsim_simulator = qsimcirq.QSimSimulator(qsim_options=gpu_options)
+        qsim_gpu_result = qsim_simulator.simulate(c)
+        print('qsim GPU result:')
+        print(qsim_gpu_result)
+        sys.stdout.flush()
+        print("")
+
+        print("python cirqbridge end")
+        sys.stdout.flush()
+        #return (result.final_state_vector)
+
+PYTHON
+
 class Cirq
   def initialize(circuit_id:, qubit_count:, step_index:, steps:, targets:)
     @circuit_id = circuit_id
@@ -8,6 +97,8 @@ class Cirq
   end
 
   def run
+    cirqbridge = PyCall.eval('cirqbridge').call
+    cirqbridge.run_simulation(@qubit_count, @steps)
     @steps.map.with_index { |each, index| execute_step each, index }
   end
 
@@ -31,8 +122,8 @@ class Cirq
         # {"type"=>"Z", "controls"=>[], "if"=>nil}
       when 'P'
         # {"type"=>"P", "phi"=>"pi/2", "controls"=>[], "targets"=>[0], "if"=>nil}
-      when 'X^½'
-        # {"type"=>"X^½", "controls"=>[], "if"=>nil}
+      when 'X^'
+        # {"type"=>"X^", "controls"=>[], "if"=>nil}
       when 'Rx'
         # {"type"=>"Rx", "theta"=>"pi/2", "controls"=>[], "targets"=>[], "if"=>nil}
       when 'Ry'
@@ -41,8 +132,8 @@ class Cirq
         # {"type"=>"Rz", "theta"=>"pi/2", "controls"=>[], "targets"=>[], "if"=>nil}
       when 'Swap'
         # {"type"=>"Swap", "controls"=>[], "targets"=>[]}
-      when '•'
-        # {"type"=>"•", "targets"=>[0]}
+      when ''
+        # {"type"=>"", "targets"=>[0]}
       when 'Bloch'
         # {"type"=>"Bloch"}
       when 'Write'
@@ -50,7 +141,9 @@ class Cirq
       when 'Measure'
         # {"type"=>"Measure", "flag"=>""}
         measured_bits[bit] = [0, 1].sample # rubocop:disable Performance/CollectionLiteralInLoop
-      when '1'
+      when '|0>'
+        # NOP
+      when '|1>'
         # NOP
       else
         raise "Unknown operation: #{each.inspect}"
