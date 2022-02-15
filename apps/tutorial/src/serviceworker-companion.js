7d3151b8e88207b8d5d604a03120b9c2b90e90f1
Yasuhito Takamiya <yasuhito@gmail.com>
2022-02-15 11:22:25 +0900

    開発環境でのサービスワーカのパスを修正

--- apps/tutorial/src/serviceworker-companion.js
+++ apps/tutorial/src/serviceworker-companion.js
@@ -1,7 +1,13 @@
 if (navigator.serviceWorker) {
-  navigator.serviceWorker
-    .register('/qni/serviceworker.js', { scope: '/qni/' })
-    .catch(function (error) {
+  if (location.hostname === 'localhost') {
+    navigator.serviceWorker.register('/serviceworker.js', { scope: './' }).catch(function (error) {
       console.log('[Companion]', 'Service worker registration failed: ' + error)
     })
+  } else {
+    navigator.serviceWorker
+      .register('/qni/serviceworker.js', { scope: '/qni/' })
+      .catch(function (error) {
+        console.log('[Companion]', 'Service worker registration failed: ' + error)
+      })
+  }
 }
