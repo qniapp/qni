8550b9e30961914f64283397e6ab6477e25c2a9c
Yasuhito Takamiya <yasuhito@gmail.com>
2022-02-28 16:55:29 +0900

    @tailwindcss/typography

--- apps/tutorial/tailwind.config.js
+++ apps/tutorial/tailwind.config.js
@@ -4,5 +4,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require('postcss-import'), require('tailwindcss')],
+  plugins: [require('@tailwindcss/typography'), require('postcss-import'), require('tailwindcss')],
 }
