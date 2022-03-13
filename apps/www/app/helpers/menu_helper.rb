d15a7eb7811eaf7c2a11c9d090dc98967b408557
Yasuhito Takamiya <yasuhito@gmail.com>
2022-03-13 10:59:05 +0900

    .

--- apps/www/app/helpers/menu_helper.rb
+++ apps/www/app/helpers/menu_helper.rb
@@ -4,14 +4,14 @@ module MenuHelper
   end
 
   def menu_item_group(&block)
-    render 'menu_item_group', &block
+    render 'menu/item_group', &block
   end
 
   def menu_item_action(options)
-    render 'menu_item_action', options: options
+    render 'menu/item_action', options: options
   end
 
   def menu_item_link(options)
-    render 'menu_item_link', options: options
+    render 'menu/item_link', options: options
   end
 end
