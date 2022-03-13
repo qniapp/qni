b5312fc291b0820eb23528708f379bf38a11dadf
Yasuhito Takamiya <yasuhito@gmail.com>
2022-03-13 10:16:28 +0900

    ヘルパを整理

--- apps/www/app/helpers/menu_helper.rb
+++ apps/www/app/helpers/menu_helper.rb
@@ -1,5 +1,17 @@
 module MenuHelper
+  def menu
+    render 'menu'
+  end
+
   def menu_item_group(&block)
     render 'menu_item_group', &block
+  end
+
+  def menu_item_action(options)
+    render 'menu_item_action', options: options
+  end
+
+  def menu_item_link(options)
+    render 'menu_item_link', options: options
   end
 end
