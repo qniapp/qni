fa513aa7ad8ecdf2fa4c7a0f07b5c8bbe16896d9
Yasuhito Takamiya <yasuhito@gmail.com>
2022-04-07 11:51:06 +0900

    add rnot-gate tests

--- packages/elements/test/rnot-gate-element.js
+++ packages/elements/test/rnot-gate-element.js
@@ -1,4 +1,5 @@
 import '../dist/index'
+import {testActivateable} from './common/test-activateable'
 import {testDisableable} from './common/test-disableable.js'
 import {testDraggableOperation} from './common/test-draggable.js'
 import {testElementCreation} from './common/test-element-creation.js'
@@ -8,6 +9,10 @@ import {testWireableOperation} from './common/test-wireable.js'
 describe('rnot-gate element', function () {
   describe('element creation', function () {
     testElementCreation(window.RnotGateElement, 'rnot-gate')
+  })
+
+  describe('activateable', function () {
+    testActivateable('rnot-gate')
   })
 
   describe('iconable', function () {
