ab85de64797320df2ea36ae739211f39701a167b
Yasuhito Takamiya <yasuhito@gmail.com>
2022-04-07 12:06:13 +0900

    add measurement-gate tests

--- packages/elements/test/measurement-gate-element.js
+++ packages/elements/test/measurement-gate-element.js
@@ -1,4 +1,5 @@
 import '../dist/index'
+import {testActivateable} from './common/test-activateable'
 import {testDraggableOperation} from './common/test-draggable'
 import {testElementCreation} from './common/test-element-creation'
 import {testIconable} from './common/test-iconable'
@@ -6,6 +7,10 @@ import {testIconable} from './common/test-iconable'
 describe('measurement-gate element', function () {
   describe('element creation', function () {
     testElementCreation(window.MeasurementGateElement, 'measurement-gate')
+  })
+
+  describe('activateable', function () {
+    testActivateable('measurement-gate')
   })
 
   describe('iconable', function () {
