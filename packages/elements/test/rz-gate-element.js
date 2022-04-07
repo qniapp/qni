1af603e5832c2134a3069e8b122d217e3095f0ed
Yasuhito Takamiya <yasuhito@gmail.com>
2022-04-07 11:58:59 +0900

    add rz-gate tests

--- packages/elements/test/rz-gate-element.js
+++ packages/elements/test/rz-gate-element.js
@@ -1,4 +1,5 @@
 import '../dist/index'
+import {testActivateable} from './common/test-activateable'
 import {testDisableable} from './common/test-disableable'
 import {testDraggableOperation} from './common/test-draggable'
 import {testElementCreation} from './common/test-element-creation'
@@ -8,6 +9,10 @@ import {testWireableOperation} from './common/test-wireable'
 describe('rz-gate element', function () {
   describe('element creation', function () {
     testElementCreation(window.RzGateElement, 'rz-gate')
+  })
+
+  describe('activateable', function () {
+    testActivateable('rz-gate')
   })
 
   describe('iconable', function () {
