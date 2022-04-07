c8d302befc822dec09aec44a2019568d5cbc6d4f
Yasuhito Takamiya <yasuhito@gmail.com>
2022-04-07 11:52:56 +0900

    add rx-gate tests

--- packages/elements/test/rx-gate-element.js
+++ packages/elements/test/rx-gate-element.js
@@ -1,4 +1,5 @@
 import '../dist/index'
+import {testActivateable} from './common/test-activateable'
 import {testDisableable} from './common/test-disableable.js'
 import {testDraggableOperation} from './common/test-draggable.js'
 import {testElementCreation} from './common/test-element-creation.js'
@@ -8,6 +9,10 @@ import {testWireableOperation} from './common/test-wireable.js'
 describe('rx-gate element', function () {
   describe('element creation', function () {
     testElementCreation(window.RxGateElement, 'rx-gate')
+  })
+
+  describe('activateable', function () {
+    testActivateable('rx-gate')
   })
 
   describe('iconable', function () {
