550c31192b18bd6f0915d6dc309b02fbce496e09
Yasuhito Takamiya <yasuhito@gmail.com>
2022-04-07 12:03:13 +0900

    add bloch-display tests

--- packages/elements/test/bloch-display-element.js
+++ packages/elements/test/bloch-display-element.js
@@ -1,10 +1,15 @@
 import '../dist/index'
+import {testActivateable} from './common/test-activateable'
 import {testDraggableOperation} from './common/test-draggable'
 import {testElementCreation} from './common/test-element-creation'
 
 describe('bloch-display element', function () {
   describe('element creation', function () {
     testElementCreation(window.BlochDisplayElement, 'bloch-display')
+  })
+
+  describe('activateable', function () {
+    testActivateable('x-gate')
   })
 
   describe('draggable', function () {
