f6a126b6d5400fbb15ad68b845765a2f4f699a86
Yasuhito Takamiya <yasuhito@gmail.com>
2022-04-07 14:23:06 +0900

    add data-hoverable tests

--- packages/elements/test/h-gate-element.js
+++ packages/elements/test/h-gate-element.js
@@ -1,4 +1,5 @@
 import '../dist/index'
+import {assert} from '@esm-bundle/chai'
 import {testActivateable} from './common/test-activateable'
 import {testDisableable} from './common/test-disableable'
 import {testDraggableOperation} from './common/test-draggable'
@@ -9,6 +10,43 @@ import {testWireableOperation} from './common/test-wireable'
 describe('h-gate element', function () {
   describe('element creation', function () {
     testElementCreation(window.HGateElement, 'h-gate')
+  })
+
+  describe('hoverable', function () {
+    beforeEach(function () {
+      const el = document.createElement('h-gate')
+      document.body.append(el)
+    })
+
+    afterEach(function () {
+      document.body.textContent = ''
+    })
+
+    it('not hoverable by default', function () {
+      const operation = document.querySelector('h-gate')
+
+      assert.isFalse(operation.hoverable)
+      assert.isFalse(operation.hasAttribute('data-hoverable'))
+    })
+
+    it('hoverable = true', function () {
+      const operation = document.querySelector('h-gate')
+
+      operation.hoverable = true
+
+      assert.isTrue(operation.hoverable)
+      assert.isTrue(operation.hasAttribute('data-hoverable'))
+    })
+
+    it('hoverable = false', function () {
+      const operation = document.querySelector('h-gate')
+      operation.hoverable = true
+
+      operation.hoverable = false
+
+      assert.isFalse(operation.hoverable)
+      assert.isFalse(operation.hasAttribute('data-hoverable'))
+    })
   })
 
   describe('activateable', function () {
