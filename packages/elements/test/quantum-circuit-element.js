47aa12ddaba6752680fd543b0b3febe78fab0179
Yasuhito Takamiya <yasuhito@gmail.com>
2022-04-08 14:38:57 +0900

    load circuit from quantum-circuit's data-json attribute

--- packages/elements/test/quantum-circuit-element.js
+++ packages/elements/test/quantum-circuit-element.js
@@ -7,6 +7,36 @@ describe('quantum-circuit element', function () {
     testElementCreation(window.QuantumCircuitElement, 'quantum-circuit')
   })
 
+  describe('quantum-circuit data-json attribute', function () {
+    afterEach(function () {
+      document.body.textContent = ''
+    })
+
+    it('renders a quantum circuit', function () {
+      const circuit = new window.QuantumCircuitElement()
+      document.body.append(circuit)
+
+      // -|0>-H-•-M-
+      //        |
+      // -|0>---X-M-
+      circuit.json = '{"cols":[["{entangle"],["|0>","|0>"],["H"],["•","X"],["}"],["Measure","Measure"]]}'
+
+      assert.equal(circuit.stepAt(0).dropzoneAt(0).operationName, 'write-gate')
+      assert.equal(circuit.stepAt(0).dropzoneAt(0).operation.value, '0')
+      assert.equal(circuit.stepAt(0).dropzoneAt(1).operationName, 'write-gate')
+      assert.equal(circuit.stepAt(0).dropzoneAt(1).operation.value, '0')
+
+      assert.equal(circuit.stepAt(1).dropzoneAt(0).operationName, 'h-gate')
+      assert.equal(circuit.stepAt(1).dropzoneAt(1).operation, null)
+
+      assert.equal(circuit.stepAt(2).dropzoneAt(0).operationName, 'control-gate')
+      assert.equal(circuit.stepAt(2).dropzoneAt(1).operationName, 'x-gate')
+
+      assert.equal(circuit.stepAt(3).dropzoneAt(0).operationName, 'measurement-gate')
+      assert.equal(circuit.stepAt(3).dropzoneAt(1).operationName, 'measurement-gate')
+    })
+  })
+
   describe('classical and quantum wires', function () {
     afterEach(function () {
       document.body.textContent = ''
