d20b6040a9e1a6674e509a5a708008270a222634
Yasuhito Takamiya <yasuhito@gmail.com>
2022-05-09 05:36:41 +0900

    リファクタリング

--- apps/www/app/assets/javascripts/serviceworker.js
+++ apps/www/app/assets/javascripts/serviceworker.js
@@ -1,7 +1,7 @@
 import {Simulator} from '@qni/simulator'
 import {Util} from '@qni/common'
 
-const handleMessage = e => {
+const runSimulator = e => {
   const qubitCount = e.data.qubitCount
   const stepIndex = e.data.stepIndex
   const steps = e.data.steps
@@ -13,23 +13,18 @@ const handleMessage = e => {
   Util.notNull(steps)
   Util.notNull(targets)
 
-  // eslint-disable-next-line github/array-foreach
-  steps.forEach((operations, i) => {
+  for (const [i, operations] of steps.entries()) {
     simulator.runStep(operations)
-
-    const message = {
+    self.postMessage({
       type: 'step',
       step: i,
       amplitudes: i === stepIndex ? simulator.amplitudes(targets) : [],
       blochVectors: simulator.blochVectors,
       measuredBits: simulator.measuredBits,
       flags: simulator.flags
-    }
-
-    self.postMessage(message)
-  })
-
+    })
+  }
   self.postMessage({type: 'finish'})
 }
 
-self.addEventListener('message', handleMessage, false)
+self.addEventListener('message', runSimulator, false)
