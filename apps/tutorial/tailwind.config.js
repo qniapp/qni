70d40d6bfefd62b889387ec7c384c6b889905117
Yasuhito Takamiya <yasuhito@gmail.com>
2022-05-28 09:10:32 +0900

    fix tailwind warnings

--- apps/tutorial/tailwind.config.js
+++ apps/tutorial/tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   mode: 'jit',
   content: ['./*.html', '_includes/*.html', '_layouts/*.html', '_plugins/*.rb/'],
+  safelist: ['w-20', 'w-12', 'w-11', 'h-12', '-mt-2.5'],
   theme: {
     extend: {},
   },
@@ -10,9 +11,4 @@ module.exports = {
     require('@tailwindcss/typography'),
     require('tailwindcss'),
   ],
-  purge: {
-    options: {
-      safelist: ['w-20', 'w-12', 'w-11', 'h-12', '-mt-2.5'],
-    },
-  },
 }
