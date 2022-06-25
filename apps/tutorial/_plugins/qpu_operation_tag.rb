b333af7e4d7ec9a88387a75f25ab709c827686b6
Yasuhito Takamiya <yasuhito@gmail.com>
2022-06-25 09:49:29 +0900

    qpu_operation タグを追加 (refs #240)

---
+++ apps/tutorial/_plugins/qpu_operation_tag.rb
@@ -0,0 +1,25 @@
+module Jekyll
+  class QpuOperationTag < Liquid::Tag
+    def initialize(tag_name, op_type, tokens)
+      super
+      @op_type = op_type.strip
+    end
+
+    def render(_context)
+      "<#{tag_name} class=\"operation-xs relative top-0.5 inline-block\"></#{tag_name}>"
+    end
+
+    def tag_name
+      case @op_type
+      when 'x'
+        'x-gate'
+      when 'y'
+        'y-gate'
+      else
+        raise "Unknown operation type: #{@op_type}"
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('qpu_operation', Jekyll::QpuOperationTag)
