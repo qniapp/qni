dabf9d8f51d235aaf90452c5e7b701d2d70413c8
Yasuhito Takamiya <yasuhito@gmail.com>
2022-07-08 16:21:30 +0900

    H, X ゲートの日本語ポップアップを修正

--- apps/tutorial/_plugins/qubit_circle_filter.rb
+++ apps/tutorial/_plugins/qubit_circle_filter.rb
@@ -14,39 +14,6 @@ module Jekyll
                   raise 'not implemented'
                 end
 
-      tw_wrapper_size = case size
-                        when 'xl'
-                          20
-                        when 'lg'
-                          16
-                        when 'base'
-                          11
-                        else
-                          raise 'not implemented'
-                        end
-
-      tw_ket_mt = case size
-                  when 'xl'
-                    '-mt-3'
-                  when 'lg'
-                    '-mt-2.5'
-                  when 'base'
-                    '-mt-1'
-                  else
-                    raise 'not implemented'
-                  end
-
-      tw_font_size = case size
-                     when 'xl'
-                       'base'
-                     when 'lg'
-                       'sm'
-                     when 'base'
-                       'xs'
-                     else
-                       raise 'not implemented'
-                     end
-
       klass = if color == ''
                 "h-#{tw_size} w-#{tw_size}"
               else
@@ -61,21 +28,16 @@ module Jekyll
       show_popup_phase = contents.include?('phase')
 
       <<~HTML
-        <div class="flex w-#{tw_wrapper_size} flex-col items-start">
-          <qubit-circle
-            class="#{klass}"
-            data-ket="#{ket}"
-            data-amplitude="#{amplitude}"
-            data-popup-template-id="qubit-circle-popup"
-            #{show_popup_header ? 'data-show-popup-header' : ''}
-            #{show_popup_amplitude ? 'data-show-popup-amplitude' : ''}
-            #{show_popup_probability ? 'data-show-popup-probability' : ''}
-            #{show_popup_phase ? 'data-show-popup-phase' : ''}
-          ></qubit-circle>
-          <div class="#{tw_ket_mt} flex w-full flex-row justify-end">
-            <span class="font-mono text-slate-500 text-#{tw_font_size}">|#{ket}⟩</span>
-          </div>
-        </div>
+        <qubit-circle
+          class="#{klass}"
+          data-ket="#{ket}"
+          data-amplitude="#{amplitude}"
+          data-popup-template-id="qubit-circle-popup"
+          #{show_popup_header ? 'data-show-popup-header' : ''}
+          #{show_popup_amplitude ? 'data-show-popup-amplitude' : ''}
+          #{show_popup_probability ? 'data-show-popup-probability' : ''}
+          #{show_popup_phase ? 'data-show-popup-phase' : ''}
+        ></qubit-circle>
       HTML
     end
   end
