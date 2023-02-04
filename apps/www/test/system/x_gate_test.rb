f271e2a528681bae57674aee50f14a761680edaa
Yasuhito Takamiya <yasuhito@hey.com>
2023-02-04 12:06:12 +0900

    リファクタリング

--- apps/www/test/system/x_gate_test.rb
+++ apps/www/test/system/x_gate_test.rb
@@ -8,13 +8,63 @@ class XGateTest < ApplicationSystemTestCase
     sleep 1
   end
 
+  # ┌───┐
+  # │ X │
+  # └───┘
+  test 'default state' do
+    x_gate = palette('X')
+
+    assert_body_background_color colors_emerald(500), x_gate
+    assert_icon_color colors_white, x_gate
+    assert_no_outline x_gate
+  end
+
+  # ╔═══╗
+  # ║ X ║
+  # ╚═══╝
+  test 'hover' do
+    x_gate = palette('X')
+
+    hover x_gate
+
+    assert_body_background_color colors_emerald(500), x_gate
+    assert_icon_color colors_white, x_gate
+    assert_outline x_gate
+  end
+
+  # ┏━━━┓
+  # ┃ X ┃
+  # ┗━━━┛
+  test 'grab' do
+    x_gate = palette('X')
+
+    grab x_gate
+
+    assert_body_background_color colors_purple(500), x_gate
+    assert_icon_color colors_white, x_gate
+    assert_no_outline x_gate
+  end
+
   #       ┌───┐
   # |0⟩───│ X │───
   #       └───┘
-  test 'apply to |0>' do
-    put_operation '|0>', step: 0, bit: 0
+  test 'apply to |0⟩' do
+    put_operation '|0⟩', step: 0, bit: 0
 
     put_operation 'X', step: 1, bit: 0
+
+    assert_qubit_circles 2
+    assert_magnitudes 0, 1
+    assert_phases 0, 0
+  end
+
+  #       ┏━━━┓
+  # |0⟩───┃ X ┃───
+  #       ┗━━━┛
+  test 'preview X|0⟩' do
+    put_operation '|0⟩', step: 0, bit: 0
+
+    hover_operation 'X', step: 1, bit: 0
 
     assert_qubit_circles 2
     assert_magnitudes 0, 1
@@ -24,27 +74,13 @@ class XGateTest < ApplicationSystemTestCase
   #       ┌───┐
   # |1⟩───│ X │───
   #       └───┘
-  test 'apply to |1>' do
-    put_operation '|1>', step: 0, bit: 0
+  test 'apply to |1⟩' do
+    put_operation '|1⟩', step: 0, bit: 0
 
     put_operation 'X', step: 1, bit: 0
 
     assert_qubit_circles 2
     assert_magnitudes 1, 0
     assert_phases 0, 0
-  end
-
-  test 'hover' do
-    x_gate = palette('X')
-    x_gate.hover
-
-    assert_outline x_gate
-  end
-
-  test 'grab' do
-    x_gate = palette('X')
-    grab x_gate
-
-    assert_no_outline x_gate
   end
 end
