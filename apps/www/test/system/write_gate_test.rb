cf1b0350689bf7a4f5ee04b5141943f647ff3d96
Yasuhito Takamiya <yasuhito@hey.com>
2023-02-06 08:29:42 +0900

    リファクタリング

--- apps/www/test/system/write_gate_test.rb
+++ apps/www/test/system/write_gate_test.rb
@@ -11,49 +11,59 @@ class WriteGateTest < ApplicationSystemTestCase
   #       ┌───┐
   # |0⟩───│ H │───|1⟩
   #       └───┘
-  test 'set a qubit state to |1>' do
-    put_operation '|0>', step: 0, bit: 0
+  test 'set a qubit state to |1⟩' do
+    put_operation '|0⟩', step: 0, bit: 0
     put_operation 'H', step: 1, bit: 0
 
-    put_operation '|1>', step: 2, bit: 1
+    put_operation '|1⟩', step: 2, bit: 1
 
     assert_magnitudes 0, 0, Math.sqrt(1.0 / 2), Math.sqrt(1.0 / 2)
     assert_phases 0, 0, 0, 0
   end
 
   #
-  # |0⟩
+  # =|0⟩-
   #
-  test 'input and output wire states change' do
-    put_operation '|0>', step: 0, bit: 0
+  test '|0⟩ transforms a classical wire into a quantum wire' do
+    put_operation '|0⟩', step: 0, bit: 0
+
+    assert_input_wire_classical step: 0, bit: 0
+    assert_output_wire_quantum step: 0, bit: 0
+  end
+
+  #
+  # =|1⟩-
+  #
+  test '|1⟩ transforms a classical wire into a quantum wire' do
+    put_operation '|1⟩', step: 0, bit: 0
 
     assert_input_wire_classical step: 0, bit: 0
     assert_output_wire_quantum step: 0, bit: 0
   end
 
   test 'preview the change in input and output wire states' do
-    hover_operation '|0>', step: 0, bit: 0
+    hover_operation '|0⟩', step: 0, bit: 0
 
     assert_input_wire_classical step: 0, bit: 0
     assert_output_wire_quantum step: 0, bit: 0
   end
 
   test 'hover' do
-    write0_gate = palette('|0>')
+    write0_gate = palette('|0⟩')
     write0_gate.hover
     assert_outline write0_gate
 
-    write1_gate = palette('|1>')
+    write1_gate = palette('|1⟩')
     write1_gate.hover
     assert_outline write1_gate
   end
 
   test 'grab' do
-    write0_gate = palette('|0>')
+    write0_gate = palette('|0⟩')
     grab write0_gate
     assert_no_outline write0_gate
 
-    write1_gate = palette('|1>')
+    write1_gate = palette('|1⟩')
     grab write1_gate
     assert_no_outline write1_gate
   end
