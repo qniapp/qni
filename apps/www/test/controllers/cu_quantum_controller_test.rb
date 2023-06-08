96a9bc6750671eb4e07c4bbfb89d9a53ef0068a1
Yasuhito Takamiya <yasuhito@hey.com>
2023-06-08 16:56:19 +0900

    テストを追加

---
+++ apps/www/test/controllers/cu_quantum_controller_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class CuQuantumControllerTest < ActionDispatch::IntegrationTest
+  test 'should get show' do
+    get cu_quantum_url
+    assert_response :success
+  end
+end
