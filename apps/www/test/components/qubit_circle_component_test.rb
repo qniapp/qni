22ca15954cb76a99323c1e24ca2bcf4b71141a24
Yasuhito Takamiya <yasuhito@hey.com>
2026-04-15 15:54:45 +0900

    chore: clean up frontend warning noise

--- apps/www/test/components/qubit_circle_component_test.rb
+++ apps/www/test/components/qubit_circle_component_test.rb
@@ -3,10 +3,19 @@
 require "test_helper"
 
 class QubitCircleComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(QubitCircleComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  def test_renders_qubit_circle_element_with_ket_and_amplitude_data
+    html = render_inline(QubitCircleComponent.new(ket: 0, amplitude: 1, dark_mode: false)).to_html
+
+    assert_match(/<qubit-circle/, html)
+    assert_match(/data-ket="0"/, html)
+    assert_match(/data-amplitude="1"/, html)
+  end
+
+  def test_sets_dark_mode_data_attribute_when_enabled
+    html = render_inline(QubitCircleComponent.new(ket: 1, amplitude: '-i', dark_mode: true)).to_html
+
+    assert_match(/data-ket="1"/, html)
+    assert_match(/data-amplitude="-i"/, html)
+    assert_match(/data-dark-mode=""/, html)
   end
 end
--- apps/www/test/components/qubit_transition_component_test.rb
+++ apps/www/test/components/qubit_transition_component_test.rb
@@ -3,10 +3,32 @@
 require "test_helper"
 
 class QubitTransitionComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(QubitTransitionComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  def test_renders_from_and_to_qubits_with_operation
+    html = render_inline(QubitTransitionComponent.new(
+      from: [1, 0],
+      to: [0.7071, 0.7071],
+      operation: :h,
+      dark_mode: false
+    )).to_html
+
+    assert_equal(4, html.scan(/<qubit-circle/).length)
+    assert_match(/data-ket="0"/, html)
+    assert_match(/data-ket="1"/, html)
+    assert_match(/data-amplitude="0\.7071"/, html)
+    assert_match(/<h-gate class="qpu-operation-sm"><\/h-gate>/, html)
+  end
+
+  def test_passes_value_to_operation_component
+    html = render_inline(QubitTransitionComponent.new(
+      from: [1, 0],
+      to: [1, 0],
+      operation: :rz,
+      angle: 'π/2',
+      value: 't',
+      dark_mode: true
+    )).to_html
+
+    assert_match(/<rz-gate class="qpu-operation-sm" data-value="t"><\/rz-gate>/, html)
+    assert_equal(4, html.scan(/data-dark-mode=""/).length)
   end
 end
